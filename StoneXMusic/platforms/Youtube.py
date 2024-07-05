ba92560f50e5d7b7b03d297ae905160c57f1e921
The-stone <147587252+the-stonex@users.noreply.github.com>
2024-07-05 08:09:12 -0700

    Update Youtube.py

--- StoneXMusic/platforms/Youtube.py
+++ StoneXMusic/platforms/Youtube.py
@@ -8,8 +8,8 @@ from pyrogram.enums import MessageEntityType
 from pyrogram.types import Message
 from youtubesearchpython.__future__ import VideosSearch
 
-from ChampuXMusic.utils.database import is_on_off
-from ChampuXMusic.utils.formatters import time_to_seconds
+from StoneXMusic.utils.database import is_on_off
+from StoneXMusic.utils.formatters import time_to_seconds
 
 
 async def shell_cmd(cmd):
