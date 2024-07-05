19b1b2cb7390f9a22e3070fc63215ae5d18235a2
The-stone <147587252+the-stonex@users.noreply.github.com>
2024-07-05 08:10:46 -0700

    Update Soundcloud.py

--- StoneXMusic/platforms/Soundcloud.py
+++ StoneXMusic/platforms/Soundcloud.py
@@ -2,7 +2,7 @@ from os import path
 
 from yt_dlp import YoutubeDL
 
-from ChampuXMusic.utils.formatters import seconds_to_min
+from StoneXMusic.utils.formatters import seconds_to_min
 
 
 class SoundAPI:
