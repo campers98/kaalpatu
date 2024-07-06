5cd95c064c25b10e6d19b3773c1e5554609b32b9
The-stone <147587252+the-stonex@users.noreply.github.com>
2024-07-05 19:31:17 -0700

    Update database.py

--- StoneXMusic/utils/database/database.py
+++ StoneXMusic/utils/database/database.py
@@ -1,8 +1,8 @@
 import random
 from typing import Dict, List, Union
 
-from ChampuXMusic import userbot
-from ChampuXMusic.core.mongo import mongodb
+from StoneXMusic import userbot
+from StoneXMusic.core.mongo import mongodb
 
 authdb = mongodb.adminauth
 authuserdb = mongodb.authuser
@@ -26,7 +26,7 @@ suggdb = mongodb.suggestion
 cleandb = mongodb.cleanmode
 queriesdb = mongodb.queries
 userdb = mongodb.userstats
-videodb = mongodb.Champuvideocalls
+videodb = mongodb.Stonevideocalls
 chatsdbc = mongodb.chatsc  # for clone
 usersdbc = mongodb.tgusersdbc  # for clone
 
@@ -191,7 +191,7 @@ async def set_assistant_new(chat_id, number):
 
 
 async def set_assistant(chat_id):
-    from ChampuXMusic.core.userbot import assistants
+    from StoneXMusic.core.userbot import assistants
 
     ran_assistant = random.choice(assistants)
     assistantdict[chat_id] = ran_assistant
@@ -205,7 +205,7 @@ async def set_assistant(chat_id):
 
 
 async def get_assistant(chat_id: int) -> str:
-    from ChampuXMusic.core.userbot import assistants
+    from StoneXMusic.core.userbot import assistants
 
     assistant = assistantdict.get(chat_id)
     if not assistant:
@@ -232,7 +232,7 @@ async def get_assistant(chat_id: int) -> str:
 
 
 async def set_calls_assistant(chat_id):
-    from ChampuXMusic.core.userbot import assistants
+    from StoneXMusic.core.userbot import assistants
 
     ran_assistant = random.choice(assistants)
     assistantdict[chat_id] = ran_assistant
@@ -245,7 +245,7 @@ async def set_calls_assistant(chat_id):
 
 
 async def group_assistant(self, chat_id: int) -> int:
-    from ChampuXMusic.core.userbot import assistants
+    from StoneXMusic.core.userbot import assistants
 
     assistant = assistantdict.get(chat_id)
     if not assistant:
