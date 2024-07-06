559b5a346fbe26ae9890cbc1a008bbfa9734fa66
The-stone <147587252+the-stonex@users.noreply.github.com>
2024-07-05 23:20:41 -0700

    Update bot.py

--- StoneXMusic/core/bot.py
+++ StoneXMusic/core/bot.py
@@ -6,11 +6,11 @@ import config
 from ..logging import LOGGER
 
 
-class Champu(Client):
+class Stone(Client):
     def __init__(self):
         LOGGER(__name__).info(f"Starting Bot...")
         super().__init__(
-            name="ChampuXMusic",
+            name="StoneXMusic",
             api_id=config.API_ID,
             api_hash=config.API_HASH,
             bot_token=config.BOT_TOKEN,
