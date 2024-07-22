4b8a110e5e1e87252e4b1d80040c60c51ba78232
campers98 <140605897+campers98@users.noreply.github.com>
2024-07-22 12:08:13 +0530

    Update config.py

--- config.py
+++ config.py
@@ -33,7 +33,7 @@ HEROKU_API_KEY = getenv("HEROKU_API_KEY")
 
 UPSTREAM_REPO = getenv(
     "UPSTREAM_REPO",
-    "https://github.com/bommugurl/stonemusic",
+    "https://github.com/campers98/kaalpatu",
 )
 UPSTREAM_BRANCH = getenv("UPSTREAM_BRANCH", "main")
 GIT_TOKEN = getenv(
